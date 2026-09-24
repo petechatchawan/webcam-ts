fd2c1dfa1a74ee088d706455dcdcf107ffa52fbb
Pete Chatchawan <pete.chatchawan@gmail.com>
2026-09-24 13:52:58 +0700

    refactor(webcam-ts): share media-devices invoke/normalize helper in adapter

--- packages/webcam-ts/src/platform/browser-media-devices-adapter.ts
+++ packages/webcam-ts/src/platform/browser-media-devices-adapter.ts
@@ -2,21 +2,28 @@ import type { MediaDevicesPort } from "./media-devices-port.js";
 import { resolveMediaDevices } from "./browser-environment.js";
 import { normalizeBrowserError } from "./browser-error-normalizer.js";
 
+async function invokeMediaDevices<T>(
+	requiredMethod: "getUserMedia" | "enumerateDevices",
+	call: (mediaDevices: MediaDevices) => Promise<T>,
+): Promise<T> {
+	try {
+		return await call(resolveMediaDevices(requiredMethod));
+	} catch (error) {
+		throw normalizeBrowserError(error);
+	}
+}
+
 export class BrowserMediaDevicesAdapter implements MediaDevicesPort {
 	async open(constraints: MediaStreamConstraints): Promise<MediaStream> {
-		try {
-			return await resolveMediaDevices("getUserMedia").getUserMedia(constraints);
-		} catch (error) {
-			throw normalizeBrowserError(error);
-		}
+		return invokeMediaDevices("getUserMedia", (mediaDevices) =>
+			mediaDevices.getUserMedia(constraints),
+		);
 	}
 
 	async enumerateDevices(): Promise<MediaDeviceInfo[]> {
-		try {
-			return await resolveMediaDevices("enumerateDevices").enumerateDevices();
-		} catch (error) {
-			throw normalizeBrowserError(error);
-		}
+		return invokeMediaDevices("enumerateDevices", (mediaDevices) =>
+			mediaDevices.enumerateDevices(),
+		);
 	}
 
 	subscribeDeviceChange(listener: () => void): () => void {
